a22c46f1a7c2a6888120dc33c600ff99c3476fac
Padefe <thegamechair@gmail.com>
2026-03-03 11:20:22 +0100

    en melding

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
-import express from "express";
+// server import
+import express from "express"; 
 import path from "path";
 import { fileURLToPath } from "url";
 
+// __dirname i ESM (Type måte å definere dynamiske filstier)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Inititierer express som en konstant
 const app = express();
 
+// Grunnleggende middleware
 app.use(express.json());
 
+// Statiske filer
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+// HTML-sidene vi bruker i prosjektet
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
+//Serveren og hvilken port den skal lytte til
 app.listen(3000, () => console.log("Server kjører på http://localhost:3000"));

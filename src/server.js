4f01491751ceacf044672bdae563cac546adb457
Padefe <thegamechair@gmail.com>
2026-03-03 12:49:10 +0100

    Lagt inn routes

--- src/server.js
+++ src/server.js
@@ -3,11 +3,14 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Routes import
+import brukerRoutes from "../src/routes/authRoutes.js";
+
 // __dirname i ESM (Type måte å definere dynamiske filstier)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Inititierer express som en konstant
+// Initierer express som en konstant
 const app = express();
 
 // Grunnleggende middleware
@@ -15,6 +18,9 @@ app.use(express.json());
 
 // Statiske filer
 app.use(express.static(path.join(__dirname, "..", "public")));
+
+// API routes, Vi bruker falske rutinger fra frontend og ruter til de riktige filstier
+app.use("/api/auth", brukerRoutes);
 
 // HTML-sidene vi bruker i prosjektet
 app.get("/", (req, res) => {
